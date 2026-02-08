5628d2c7d5e198dda414b33a9eec9aa133bd8e8d
DevNom7 <nomx.0492@gmail.com>
2026-02-07 20:23:04 -0500

    Module 5 REST API wired up

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,20 +1,20 @@
+// app_api/models/db.js
 const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1';
-const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readline');
 
-// Build the connection string and set the connection timeout
-const connect = () => {
-  setTimeout(() => mongoose.connect(dbURI, {}), 1000);
-};
+const host = process.env.DB_HOST || '127.0.0.1';
+const dbURI = `mongodb://${host}/travlr`;
+
+// Connect (simple + reliable)
+mongoose.connect(dbURI);
 
 // Monitor connection events
 mongoose.connection.on('connected', () => {
   console.log(`Mongoose connected to ${dbURI}`);
 });
 
-mongoose.connection.on('error', err => {
-  console.log('Mongoose connection error: ', err);
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection error:', err);
 });
 
 mongoose.connection.on('disconnected', () => {
@@ -33,30 +33,33 @@ if (process.platform === 'win32') {
   });
 }
 
-// Graceful shutdown
-const gracefulShutdown = (msg) => {
+// Graceful shutdown helper
+const gracefulShutdown = (msg, callback) => {
   mongoose.connection.close(() => {
     console.log(`Mongoose disconnected through ${msg}`);
+    callback();
   });
 };
 
 process.once('SIGUSR2', () => {
-  gracefulShutdown('nodemon restart');
-  process.kill(process.pid, 'SIGUSR2');
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
 });
 
 process.on('SIGINT', () => {
-  gracefulShutdown('app termination');
-  process.exit(0);
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
 });
 
 process.on('SIGTERM', () => {
-  gracefulShutdown('app shutdown');
-  process.exit(0);
+  gracefulShutdown('app shutdown', () => {
+    process.exit(0);
+  });
 });
 
-// Connect + load schema
-connect();
+// Rregister schemas/models here
 require('./travlr');
 
 module.exports = mongoose;
